6bd9f9931bf9081b7b0cff052c5da1950dd03a48
phuc98ute <vophuc3+copilot@gmail.com>
2024-09-19 13:43:33 +0700

    Init

---
+++ src/main/java/vn/phucvo/dependabotignoremajor/DependabotIgnoreMajorApplication.java
@@ -0,0 +1,13 @@
+package vn.phucvo.dependabotignoremajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotIgnoreMajorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependabotIgnoreMajorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/phucvo/dependabotignoremajor/DependabotIgnoreMajorApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.phucvo.dependabotignoremajor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotIgnoreMajorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
